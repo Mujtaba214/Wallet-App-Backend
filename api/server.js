db435ba79b3e442bc6bbbe250ec84bc54e85d4e5
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-12-15 14:06:46 +0500

    file updated

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import { initDB } from "../config/db.js";
 import { rateLimiter } from "../middleware/rateLimiter.js";
 import transactionRoutes from "../routes/transactionRoutes.js";
 
@@ -10,28 +9,14 @@ dotenv.config();
 
 const app = express();
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
-// routes
 app.use("/api/transactions", transactionRoutes);
 
-// health check
 app.get("/", (req, res) => {
-  res.send("API running on Vercel");
+  res.send("API running on Vercel + Neon");
 });
 
-// ⚠️ IMPORTANT: DB init WITHOUT listen
-let dbInitialized = false;
-
-app.use(async (req, res, next) => {
-  if (!dbInitialized) {
-    await initDB();
-    dbInitialized = true;
-  }
-  next();
-});
-
-export default app; // ✅ REQUIRED FOR VERCEL
+export default app;
