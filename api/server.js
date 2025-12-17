aa461599d0c787b3c648e5971abce91998084e70
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-12-17 13:51:24 +0500

    changes in server.js

--- api/server.js
+++ api/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // allow all for mobile apps
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
 
